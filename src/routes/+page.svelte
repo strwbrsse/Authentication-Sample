87f4a98ead2874e246b53687bcb6eaf231876ab6
Caresse <caressem12@gmail.com>
2026-07-11 09:36:23 +0800

    Login Screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<div class="border-slate-200 border-2 rounded-lg shadow-slate-200 shadow-lg p-4 max-w-25">
+<div class="border-slate-200 border-2 rounded-lg shadow-slate-200 shadow-lg p-4 max-w-2xl mx-auto">
+
+	<h1 class="text-xl format-semibold text-center">Login</h1>
+
+	<fieldset class="fieldset">
+		<legend class="fieldset-legend">Email</legend>
+		<input type="email" class="input" placeholder="Type your email" />
+	</fieldset>
+
+  <fieldset class="fieldset">
+		<legend class="fieldset-legend">Password</legend>
+		<input type="password" class="input" placeholder="Type your password" />
+	</fieldset>
 
 </div>
